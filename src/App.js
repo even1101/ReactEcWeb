eaed125ae06f9160bb6facb2b03bea11964e2c2a
even <yesee01.test@yesee.com.tw>
2021-06-14 19:06:22 +0800

    Added shopPage collection-preview and collection-item

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import HomePage from './pages/home-page/home-page.component';
-
-const HatsPage = () => {
-  return(
-    <div><h1>Hats  Page</h1></div>
-  )
-}
-
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/home-page/home-page.component";
+import ShopPage from "./pages/shop-page/shop-page.component";
 
 function App() {
   return (
     <div>
       <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop/hats' component={HatsPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={ShopPage} />
       </Switch>
-    </div>  
+    </div>
   );
 }
 
